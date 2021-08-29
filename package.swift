ae6cd1c84a26ffdbafe6adcb80a409eeea078dac
Steve Hume <stevelist@hume.ca>
2021-08-29 00:46:10 +0000

    with dependencies

--- package.swift
+++ package.swift
@@ -13,14 +13,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+       .package(url:"https://github.com/johnsundell/publish.git", from: "0.8.0"),
+       .package(name: "ShellOut", url: "https://github.com/johnsundell/shellout.git", from: "2.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CompressPublishPlugin",
-            dependencies: []),
+            dependencies: ["Publish","ShellOut"]),
         .testTarget(
             name: "CompressPublishPluginTests",
             dependencies: ["CompressPublishPlugin"]),
