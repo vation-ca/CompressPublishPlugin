efe5f58151c00e3a4317ae59492bb5579749bf2c
Steve Hume <stevelist@hume.ca>
2021-08-29 00:34:59 +0000

    clean package.swift

--- package.swift
+++ package.swift
@@ -1,29 +1,28 @@
-// swift-tools-version:5.1
-
-/**
-*  Compress-plugin for Publish
-*  Copyright (c) Vation Inc. 2021
-*  MIT license, see LICENSE file for details
-*/
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-	name: "CompressPublishPlugin",
-	products: [
-		.library(
-			name: "CompressPublishPlugin",
-			targets: ["CompressPublishPlugin"]
-		)
-	],
-	dependencies: [
-		.package(url: "https://github.com/johnsundell/publish.git", from: "0.8.0"),
-		.package(name: "ShellOut", url: "https://github.com/johnsundell/shellout.git", from: "2.3.0")
-	],
-	targets: [
-		.target(
-			name: "CompressPublishPlugin",
-			dependencies: ["Publish","ShellOut"]
-		)
-	]
+    name: "CompressPublishPlugin",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "CompressPublishPlugin",
+            targets: ["CompressPublishPlugin"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "CompressPublishPlugin",
+            dependencies: []),
+        .testTarget(
+            name: "CompressPublishPluginTests",
+            dependencies: ["CompressPublishPlugin"]),
+    ]
 )
