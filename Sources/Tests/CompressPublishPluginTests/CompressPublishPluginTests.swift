7b6f5b5da4709fdf5a34389c26831381f4914c23
Steve Hume <stevelist@hume.ca>
2021-08-29 00:20:14 +0000

    Add empty test for now

---
+++ Sources/Tests/CompressPublishPluginTests/CompressPublishPluginTests.swift
@@ -0,0 +1,13 @@
+import Publish
+import XCTest
+@testable import CompressPublishPlugin
+
+final class CompressPublishPluginTests: XCTestCase {
+	func testExample() {
+		XCTAssertEqual("Hello, World!", "Hello, World!")
+	}
+
+	static var allTests = [
+		("testExample", testExample),
+	]
+}
